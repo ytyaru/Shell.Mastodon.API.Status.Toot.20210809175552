6b605e0d0d088cab5f743b60e7a310fc0dd6573d
ytyaru <ytyaru@outlook.jp>
2021-08-11 17:38:53 +0900

    add toot_vim.sh

--- src/memo.md
+++ src/memo.md
@@ -28,4 +28,33 @@
 
 * [Mastodon API で「返信」をトゥートする 1 行サンプル（bash, PHP）](https://qiita.com/KEINOS/items/68ae99c7f98fe309f9bf)
 
+# status API
 
+　Tootするときはstatus APIを使う。色々な機能がある。
+
+* https://docs.joinmastodon.org/methods/statuses/#deprecated-methods
+
+引数|型|概要
+----|--|----
+`status`|string|Toot本文。
+`in_reply_to_id`|string|指定したTootIDへのリプライ(返信)するときに指定する。
+`visibility`|string|公開範囲。`public`, `unlisted`, `private`, `direct`。
+`sensitive`|boolean|警告するか否か。
+`spoiler_text`|警告テキスト。
+`language`|言語コード。ISO639。
+
+引数|型|概要
+----|--|----
+`scheduled_at`|string|予約投稿。最低でも5分後であるべき。`2019-12-05T12:33:01.000Z`, `2019-12-05T12:33:01.000+0900`
+
+引数|型|概要
+----|--|----
+`media_ids`|array|画像、音声、動画のID。[media][] APIのレスポンスから得られる。
+
+引数|型|概要
+----|--|----
+`poll[options]`|array|可能な答えの配列。`poll[expires_in]`を指定せねばならない。
+`poll[expires_in]`|number|有効な時間(秒)。
+`poll[multiple]`|boolean|複数の質問をするか否か。
+
+* [media](https://docs.joinmastodon.org/methods/statuses/media/)
---
+++ src/toot_vim.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -Ceu
+#---------------------------------------------------------------------------
+# MastodonでTootする。
+# CreatedAt: 2021-08-09
+#---------------------------------------------------------------------------
+Run() {
+	THIS="$(realpath "${BASH_SOURCE:-0}")"; HERE="$(dirname "$THIS")"; PARENT="$(dirname "$HERE")"; THIS_NAME="$(basename "$THIS")"; APP_ROOT="$PARENT";
+	cd "$HERE"
+	QuickEdit() (  trap 'rm /tmp/work/temp$$' exit; vim /tmp/work/temp$$ >/dev/tty; cat /tmp/work/temp$$ )
+	UrlEncode() { python -c 'import sys, urllib; print urllib.quote(sys.stdin.read()),'; }
+	AccessToken() { cat 'token.txt'; }
+	MASTODON_HOST='mstdn.jp'
+	ACCESS_TOKEN="$(AccessToken)"
+	Toot() {
+		TEXT="$(cat -)"
+		STATUS="$(echo -e "$TEXT" | UrlEncode)"
+		curl -X POST -Ss https://${MASTODON_HOST}/api/v1/statuses \
+		  --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+		  -d "status=${STATUS}"
+	}
+	QuickEdit | Toot
+}
+Run "$@"
